34082bfcd650f5fd577283c9f3d148bc6649762c
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-09-22 10:27:46 +0300

    compete the task

--- FUND_Lectures_and_exercises/01_exercise/12_cristmal_spirit.py
+++ FUND_Lectures_and_exercises/01_exercise/12_cristmal_spirit.py
@@ -1,2 +1,28 @@
 quantity_of_decorations = int(input())
 days = int(input())
+
+total_cost = 0
+total_spirit = 0
+
+for day in range(1, days + 1):
+    if day % 11 == 0:
+        quantity_of_decorations += 2
+    if day % 2 == 0:
+        total_cost += (quantity_of_decorations*2)
+        total_spirit += 5
+    if day % 3 == 0:
+        total_cost += (quantity_of_decorations * 5) + (quantity_of_decorations * 3)
+        total_spirit += 3 + 10
+    if day % 5 == 0:
+        total_cost += (quantity_of_decorations * 15)
+        total_spirit += 17
+        if day % 3 == 0:
+            total_spirit += 30
+    if day % 10 == 0:
+        total_spirit -= 20
+        total_cost += 23
+    if day == days and day % 10 == 0:
+        total_spirit -= 30
+
+print(f"Total cost: {total_cost}")
+print(f"Total spirit: {total_spirit}")
