26a6af751becba1aaeed4d7baaf50846bbd6a9a1
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-09-29 19:00:25 +0300

    Update faro_shuffle_05.py

--- FUND_Lectures_and_exercises/03_ListsBasicsExercise/faro_shuffle_05.py
+++ FUND_Lectures_and_exercises/03_ListsBasicsExercise/faro_shuffle_05.py
@@ -1,30 +1,14 @@
 cards = input().split(" ")
-count_iterations = int(input())
-for _ in range(count_iterations):
-    first_half = []
-    second_half = []
+shuffle = int(input())
+for _ in range(shuffle):
+    final_deck = []
+    middle = len(cards) // 2
+    left_part = cards[:middle]
+    right_part = cards[middle:]
 
-# ace two three four five six
-# 1
-    for idx in range(1, len(cards) - 1):
-        curr_card = cards[idx]
-        if idx < len(cards) / 2:
-            first_half.append(curr_card)
-        else:
-            second_half.append(curr_card)
+    for i in range(len(left_part)):
+        final_deck.append(left_part[i])
+        final_deck.append(right_part[i])
+    cards = final_deck
 
-    mixed_cards = []
-    first_half_idx = 0
-    second_half_idx = 0
-    for i in range(len(first_half) * 2):
-        if i % 2 == 0:
-
-            mixed_cards.append(second_half[second_half_idx])
-            second_half_idx += 1
-        else:
-
-            mixed_cards.append(first_half[first_half_idx])
-            first_half_idx += 1
-
-    cards = [cards[0]] + mixed_cards + [cards[-1]]
 print(cards)
