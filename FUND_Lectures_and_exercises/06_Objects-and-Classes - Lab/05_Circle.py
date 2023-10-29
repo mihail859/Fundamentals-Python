516839a63356ac1ddfb49561911653779c3cae8d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-10-29 22:14:16 +0200

    Update 05_Circle.py

--- FUND_Lectures_and_exercises/06_Objects-and-Classes - Lab/05_Circle.py
+++ FUND_Lectures_and_exercises/06_Objects-and-Classes - Lab/05_Circle.py
@@ -17,3 +17,11 @@ class Circle:
         #A = (θ/360) * π * r^2
         area_sector = (angle / 360) * Circle.__pi * math.pow((self.diameter / 2), 2)
         return  area_sector
+
+
+circle = Circle(10)
+angle = 5
+
+print(f"{circle.calculate_circumference():.2f}")
+print(f"{circle.calculate_area():.2f}")
+print(f"{circle.calculate_area_of_sector(angle):.2f}")
