bc6f3a8c1cd7c397ac70ee927eb113fb6f5f6464
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-10-31 21:49:58 +0200

    Update 08_Vehicle.py

--- FUND_Lectures_and_exercises/06_Exercise-Classes-&-Objects/08_Vehicle.py
+++ FUND_Lectures_and_exercises/06_Exercise-Classes-&-Objects/08_Vehicle.py
@@ -1,26 +1,40 @@
 class Vehicle:
 
-    def __init__(self, vehicle_type, model, price):
-        self.vehicle_type = vehicle_type
+    def __init__(self, type, model, price, owner=None):
+        self.type = type
         self.model = model
         self.price = price
-        self.owner_car = None
+        self.owner = owner
 
     def buy(self, money: int, owner: str):
-        if self.owner_car is None:
+        if self.owner is not None:
             return "Car already sold"
         elif money < self.price:
             return "Sorry, not enough money"
         else:
-            self.owner_car = owner
+            self.owner = owner
             change = money - self.price
-            return f"Successfully bought a {self.vehicle_type}. Change: {change}"
+            return f"Successfully bought a {self.type}. Change: {change:.2f}"
 
     def sell(self):
-        if self.owner_car is not None:
-            self.owner_car = None
-            return True
+        if self.owner is not None:
+            self.owner = None
+
         else:
             return "Vehicle has no owner"
 
-    
+    def __repr__(self):
+        if self.owner is None:
+            return f"{self.model} {self.type} is on sale: {self.price}"
+        return f"{self.model} {self.type} is owned by: {self.owner}"
+
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
