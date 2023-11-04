3cd57f3f37664262bb7800d2782674564ee93650
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-04 22:39:31 +0200

    Create 04_phonebook.py

---
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/04_phonebook.py
@@ -0,0 +1,35 @@
+contacts = {}
+n = None
+while True:
+    input_data = input().split("-")
+    if input_data[0].isdigit():
+        n = int(input_data[0])
+        break
+    name = input_data[0]
+    phone_number= input_data[1]
+
+    contacts[name] = phone_number
+
+for i in range(n):
+    searched_contact = input()
+    if searched_contact in contacts.keys():
+        print(f"{searched_contact} -> {contacts[searched_contact]}")
+    else:
+        print(f"Contact {searched_contact} does not exist.")contacts = {}
+n = None
+while True:
+    input_data = input().split("-")
+    if input_data[0].isdigit():
+        n = int(input_data[0])
+        break
+    name = input_data[0]
+    phone_number= input_data[1]
+
+    contacts[name] = phone_number
+
+for i in range(n):
+    searched_contact = input()
+    if searched_contact in contacts.keys():
+        print(f"{searched_contact} -> {contacts[searched_contact]}")
+    else:
+        print(f"Contact {searched_contact} does not exist.")
