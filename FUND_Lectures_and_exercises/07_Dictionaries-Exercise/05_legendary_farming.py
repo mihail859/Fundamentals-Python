8833c7dc364d78c3bb4c2cc1282105d7426af1bd
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-05 21:01:48 +0200

    Update 05_legendary_farming.py

--- FUND_Lectures_and_exercises/07_Dictionaries-Exercise/05_legendary_farming.py
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/05_legendary_farming.py
@@ -3,28 +3,37 @@ farming_items = {
     "fragments": 0,
     "motes": 0
 }
-
+input_data = input().split()
 is_obtained = False
-while True:
-    input_data = input().split()
-    for i in range(len(input_data), 2):
+while not is_obtained:
+
+    for i in range(0, len(input_data), 2):
         quantity = int(input_data[i])
         material = input_data[i + 1].lower()
         if material not in farming_items.keys():
             farming_items[material] = 0
         farming_items[material] += quantity
         if farming_items["shards"] >= 250:
+            farming_items["shards"] -= 250
             is_obtained = True
             print("Shadowmourne obtained!")
-            for key, value in farming_items.keys():
-                if key != "shards":
-                    print(f"{key}: {value}")
+            for key, value in farming_items.items():
+                print(f"{key}: {value}")
             break
         if farming_items["fragments"] >= 250:
             is_obtained = True
+            farming_items["fragments"] -= 250
             print("Valanyr obtained!")
-            for key, value in farming_items.keys():
-                if key != "fragments":
-                    print(f"{key}: {value}")
+            for key, value in farming_items.items():
+                print(f"{key}: {value}")
             break
-        if    
+        if farming_items["motes"] >= 250:
+            is_obtained = True
+            farming_items["motes"] -= 250
+            print("Dragonwrath obtained!")
+            for key, value in farming_items.items():
+                print(f"{key}: {value}")
+            break
+    if is_obtained:
+        break
+    input_data = input().split()
