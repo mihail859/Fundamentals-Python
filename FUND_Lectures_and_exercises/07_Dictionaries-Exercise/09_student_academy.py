dadeeb2b3ecee955fa47cff189d4eaf7624f1380
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-07 19:57:26 +0200

    Update 09_student_academy.py

--- FUND_Lectures_and_exercises/07_Dictionaries-Exercise/09_student_academy.py
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/09_student_academy.py
@@ -1,1 +1,9 @@
 n = int(input())
+
+students_dict_info = dict()
+for i in range(n):
+    name = input()
+    grade = int(input())
+    if name not in students_dict_info.keys():
+        students_dict_info[name] = []
+    students_dict_info[name].append(grade)
