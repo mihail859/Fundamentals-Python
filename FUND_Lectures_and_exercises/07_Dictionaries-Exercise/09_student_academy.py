e922d95ce182a9125945b27601f793bcf5119e4c
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-07 20:00:22 +0200

    Update 09_student_academy.py

--- FUND_Lectures_and_exercises/07_Dictionaries-Exercise/09_student_academy.py
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/09_student_academy.py
@@ -3,7 +3,11 @@ n = int(input())
 students_dict_info = dict()
 for i in range(n):
     name = input()
-    grade = int(input())
+    grade = float(input())
     if name not in students_dict_info.keys():
         students_dict_info[name] = []
     students_dict_info[name].append(grade)
+
+for key, val in students_dict_info.items():
+    avg_score = sum(val) / len(val)
+    print(f"{key} -> {avg_score:.2f}")
