053d6b0cc627d0e2d3588eafa1ac27a44c5c450a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-09 21:24:28 +0200

    Update 11_force_Book.py

--- FUND_Lectures_and_exercises/07_Dictionaries-Exercise/11_force_Book.py
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/11_force_Book.py
@@ -6,13 +6,23 @@ def first(dictionary, side, user):
         dictionary[side] = [user]
     else:
         dictionary[side].append(user)
+    return dictionary
 
 
 def second(dictionary, user, side):
     for key, value in dictionary.values():
         if user in value:
             dictionary[key].pop(value.index(user))
-            
+            return dictionary
+    # if force_side not in force_side_dictionary.keys():
+    #     force_side_dictionary[force_side] = [force_user]
+    # else:
+    #     force_side_dictionary[force_side].append(force_user)
+    if side not in dictionary.keys():
+        dictionary[side] = [user]
+    else:
+        dictionary[side].append(user)
+    return dictionary
 
 
 force_dict = dict()
@@ -27,7 +37,7 @@ while True:
         force_user = split_line[1]
         force_dict = first(force_dict, force_side, force_user)
     elif "->" in input_line:
-        #a "force_user -> force_side"
+        # a "force_user -> force_side"
         split_line = input_line.split(" -> ")
         force_user = split_line[0]
         force_side = split_line[1]
