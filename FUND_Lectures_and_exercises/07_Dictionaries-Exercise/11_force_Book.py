8949c6388e857e2c349a9430ac4c99e7e238027a
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-09 21:07:58 +0200

    Update 11_force_Book.py

--- FUND_Lectures_and_exercises/07_Dictionaries-Exercise/11_force_Book.py
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/11_force_Book.py
@@ -12,6 +12,10 @@ while True:
     if input_line == 'Lumpawaroo':
         break
     if "|" in input_line:
-        pass
+        #/*"{force_side} | {force_user}"*/
+        split_line = input_line.split(" | ")
+        force_side = split_line[0]
+        force_user = split_line[1]
+        force_dict = first(force_dict, force_side, force_user)
     elif "->" in input_line:
         pass
