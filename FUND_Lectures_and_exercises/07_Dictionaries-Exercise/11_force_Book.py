e3de35efe7457d02081db9a97a00e5b120ceee01
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-09 21:06:11 +0200

    Update 11_force_Book.py

---
+++ FUND_Lectures_and_exercises/07_Dictionaries-Exercise/11_force_Book.py
@@ -0,0 +1,17 @@
+def first(dictionary, side, user):
+    pass
+
+
+def second(dictionary, user, side):
+    pass
+
+
+force_dict = dict()
+while True:
+    input_line = input()
+    if input_line == 'Lumpawaroo':
+        break
+    if "|" in input_line:
+        pass
+    elif "->" in input_line:
+        pass
