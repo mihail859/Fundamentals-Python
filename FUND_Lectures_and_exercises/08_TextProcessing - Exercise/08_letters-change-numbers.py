578fd9935b436aa1deeb6c5371724f136aed8d90
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-15 18:41:25 +0200

    Update 08_letters-change-numbers.py

--- FUND_Lectures_and_exercises/08_TextProcessing - Exercise/08_letters-change-numbers.py
+++ FUND_Lectures_and_exercises/08_TextProcessing - Exercise/08_letters-change-numbers.py
@@ -1,3 +1,22 @@
 input_line = input().split()
+
+total_sum = 0
+
 for sequence in input_line:
-    
+    current_sum = 0
+    if sequence[0].isupper():
+        letter_position = ord(sequence[0]) - 64
+        number = int(sequence[1:-1])
+        current_sum += number / letter_position
+    if sequence[0].islower():
+        letter_position = ord(sequence[0]) - 96
+        number = int(sequence[1:-1])
+        current_sum += number * letter_position
+    if sequence[-1].isupper():
+        letter_position = ord(sequence[0]) - 64
+        number = int(sequence[1:-1])
+        current_sum += number - letter_position
+    if sequence[-1].islower():
+        letter_position = ord(sequence[0]) - 96
+        number = int(sequence[1:-1])
+        current_sum += number * letter_position
