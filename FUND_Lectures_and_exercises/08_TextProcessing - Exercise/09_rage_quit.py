52c410219788f66346d7880aefd6e18f98b0fd4d
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-16 19:04:19 +0200

    Update 09_rage_quit.py

--- FUND_Lectures_and_exercises/08_TextProcessing - Exercise/09_rage_quit.py
+++ FUND_Lectures_and_exercises/08_TextProcessing - Exercise/09_rage_quit.py
@@ -1,4 +1,9 @@
 string_input = input()
+output = ''
+pattern = ""
 for i in range(len(string_input)):
     current_ch = string_input[i]
-    
+    if not current_ch.isdigit():
+        pattern += current_ch
+    if current_ch.isdigit() and string_input[i+1].isdigit():
+        output += ""
