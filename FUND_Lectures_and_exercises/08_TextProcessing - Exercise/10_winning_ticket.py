df74607040cc9fae83f28ae07442c53dd975a219
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-17 19:17:39 +0200

    Update 10_winning_ticket.py

--- FUND_Lectures_and_exercises/08_TextProcessing - Exercise/10_winning_ticket.py
+++ FUND_Lectures_and_exercises/08_TextProcessing - Exercise/10_winning_ticket.py
@@ -1,2 +1,6 @@
 lottery_tickets = input().split(", ")
-valid_tickets = []
+for ticket in lottery_tickets:
+    if not len(ticket) == 20:
+        print("invalid ticket")
+    else:
+        print("valid ticket")
