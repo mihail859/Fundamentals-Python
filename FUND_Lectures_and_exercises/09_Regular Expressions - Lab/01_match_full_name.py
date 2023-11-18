0d5adacfeec1e488a3e641f516311e6d5ff76ed7
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-18 15:42:12 +0200

    Update 01_match_full_name.py

--- FUND_Lectures_and_exercises/09_Regular Expressions - Lab/01_match_full_name.py
+++ FUND_Lectures_and_exercises/09_Regular Expressions - Lab/01_match_full_name.py
@@ -1,5 +1,5 @@
 import re
-pattern = r"\b[A-Z][a-z]{2,}\s[A-Z][a-z]{2,}"
+pattern = r"\b[A-Z][a-z]+ [A-Z][a-z]+\b"
 input_name = input()
 result = re.findall(pattern, input_name)
 print(" ".join(result))
