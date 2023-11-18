7be5e470bff7fc1978b65e3751f9ae434e026aff
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-18 20:12:17 +0200

    Update 02_match_phone_number.py

---
+++ FUND_Lectures_and_exercises/09_Regular Expressions - Lab/02_match_phone_number.py
@@ -0,0 +1,11 @@
+import re
+
+pattern = r'\+359([\s|-])2\1[0-9]{3}\1[0-9]{4}\b'
+numbers = input()
+matches = re.finditer(pattern, numbers)
+
+valid_numbers = []
+for match in matches:
+    valid_numbers.append(match.group(0))
+
+print(valid_numbers)
