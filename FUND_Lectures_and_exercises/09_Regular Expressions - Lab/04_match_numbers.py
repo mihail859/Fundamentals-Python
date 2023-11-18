08fe68a7397ea8974e6451cffa7e959a35316d2b
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-18 20:53:54 +0200

    Update 04_match_numbers.py

--- FUND_Lectures_and_exercises/09_Regular Expressions - Lab/04_match_numbers.py
+++ FUND_Lectures_and_exercises/09_Regular Expressions - Lab/04_match_numbers.py
@@ -1,3 +1,8 @@
 import re
 
-pattern = r'"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"gm'
+pattern = r'(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))'
+text = input()
+result = re.finditer(pattern, text)
+
+for match in result:
+    print(match.group(), end=' ')
