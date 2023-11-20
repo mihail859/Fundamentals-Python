7e8e981942e683ae105c30eb65f92756ae41a221
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-20 21:35:46 +0200

    Update 04_extact_emails.py

--- FUND_Lectures_and_exercises/09_Exercise_Regular Expressions/04_extact_emails.py
+++ FUND_Lectures_and_exercises/09_Exercise_Regular Expressions/04_extact_emails.py
@@ -1,5 +1,7 @@
 import re
 
 input_line = input()
-pattern = r''
-result = re.search(pattern, input_line)
+pattern = r'([a-z0-9]+[a-z0-9\.\-\_])@([a-z\-]+)([a-z0-9\.]+)\b'
+result = re.finditer(pattern, input_line)
+for match in result:
+    print(match.group(0))
