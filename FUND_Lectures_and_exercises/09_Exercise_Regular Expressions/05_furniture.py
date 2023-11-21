b54f0111b4b176b0e005fd58da557de6d4687d4c
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-21 19:40:03 +0200

    Update 05_furniture.py

--- FUND_Lectures_and_exercises/09_Exercise_Regular Expressions/05_furniture.py
+++ FUND_Lectures_and_exercises/09_Exercise_Regular Expressions/05_furniture.py
@@ -1,3 +1,22 @@
 import re
 
+pattern = r'>>(?P<furniture>[A-Za-z]+)<<(?P<price>\d+\.?\d*)!(?P<quantity>\d+)'
+total_price = 0
+bought_furniture = []
 
+line = input()
+while line != "Purchase":
+    result = re.match(pattern, line)
+
+    if result:
+        furniture = result.group("furniture")
+        price = float(result.group("price"))
+        quantity = int(result.group("quantity"))
+
+        bought_furniture.append(furniture)
+        total_price += price * quantity
+
+    line = input()
+
+print(bought_furniture)
+print(total_price)
