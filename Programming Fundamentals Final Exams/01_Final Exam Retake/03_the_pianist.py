8a4730aed134a65137b1c2925e51323d8c45f910
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-22 23:19:53 +0200

    Update 03_the_pianist.py

--- Programming Fundamentals Final Exams/01_Final Exam Retake/03_the_pianist.py
+++ Programming Fundamentals Final Exams/01_Final Exam Retake/03_the_pianist.py
@@ -1,6 +1,28 @@
+def add(dictionary, piece_name, composer_name, key_sign):
+    if piece in dictionary.keys():
+        print()
+
+
 number_of_pieces = int(input())
 dictionary_pieces = dict()
+
 for i in range(number_of_pieces):
     piece, composer, key = input().split("|")
     dictionary_pieces[piece] = {'composer': composer, 'key': key}
-print(dictionary_pieces)
+
+while True:
+    command, *params = input().split("|")
+    if command == 'Stop':
+        break
+    elif command == 'Add':
+        new_piece = params[0]
+        new_composer = params[1]
+        new_key = params[2]
+        dictionary_pieces = add(dictionary_pieces, new_piece, new_composer, new_key)
+
+
+    elif command == 'Remove':
+        pass
+
+    elif command == 'ChangeKey':
+        pass
