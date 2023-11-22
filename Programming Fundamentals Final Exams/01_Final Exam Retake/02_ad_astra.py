b1cf230b5cf4f651b64041f61d99ba35891437f2
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-22 21:47:07 +0200

    Update 02_ad_astra.py

--- Programming Fundamentals Final Exams/01_Final Exam Retake/02_ad_astra.py
+++ Programming Fundamentals Final Exams/01_Final Exam Retake/02_ad_astra.py
@@ -1,4 +1,25 @@
 import re
 
-pattern = r'(\||#)(?P<item>[a-zA-Z ]+)\1(?P<date>\d{2}\/[0-9]{2}\/\d{2})\1(?P<calories>\d{0,4})'
+pattern = r'(\||#)(?P<item>[a-zA-Z\s]+)\1(?P<date>\d{2}/\d{2}/\d{2})\1(?P<calories>\d{1,5})\1'
 
+foods_input = input()
+result = re.finditer(pattern, foods_input)
+
+sum_calories = 0
+product_list = []
+if result:
+
+    for match in result:
+        product = match.group('item')
+        expiration_date = match.group('date')
+        calories = int(match.group('calories'))
+        sum_calories += calories
+        a = f"Item: {product}, Best before: {expiration_date}, Nutrition: {calories}"
+        product_list.append(a)
+
+
+days = sum_calories // 2000
+print(f"You have food to last you for: {days} days!")
+if product_list:
+    for i in product_list:
+        print(i)
