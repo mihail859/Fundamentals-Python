2fa66b0d0d6ee753355992b443833592292829b6
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-27 21:16:53 +0200

    Update 01_password-reset.py

--- Programming Fundamentals Final Exams/04_Final Exam/01_password-reset.py
+++ Programming Fundamentals Final Exams/04_Final Exam/01_password-reset.py
@@ -4,7 +4,18 @@ def take_odd(line):
 
 
 def cut_function(line, idx, len_line):
-    pass
+    substring = line[idx:idx + len_line]
+    line = line.replace(substring, '')
+    return line
+
+
+def substitute(line, substring, replacement):
+    if substring not in line:
+        print("Nothing to replace!")
+    else:
+        line = line.replace(substring, replacement)
+        print(line)
+    return line
 
 
 input_line = input()
@@ -17,3 +28,9 @@ while True:
     elif command == 'Cut':
         index = int(params[0])
         length = int(params[1])
+        input_line = cut_function(input_line, index, length)
+        print(input_line)
+
+    elif command == 'Substitute':
+        substr = params[0]
+        substitute = params[1]
