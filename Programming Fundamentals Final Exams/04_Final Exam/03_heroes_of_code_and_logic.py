af97d6a4e4e5eeefc1b99b100074aa47f3a870f3
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-29 19:28:35 +0200

    Update 03_heroes_of_code_and_logic.py

--- Programming Fundamentals Final Exams/04_Final Exam/03_heroes_of_code_and_logic.py
+++ Programming Fundamentals Final Exams/04_Final Exam/03_heroes_of_code_and_logic.py
@@ -1,3 +1,19 @@
+def cast_spell(dictionary, hero, mp, spell):
+    if dictionary[hero]['mp'] > mp:
+        dictionary[hero]['mp'] -= mp
+        left_mp = dictionary[hero]['mp']
+        print(f"{hero} has successfully cast {spell} and now has {left_mp} MP!")
+
+    else:
+        print(f"{hero} does not have enough MP to cast {spell}!")
+
+    return dictionary
+
+
+def take_damage(dictionary, hero, damage, attacker):
+    pass
+
+
 n = int(input())
 heroes_dict = dict()
 
@@ -8,4 +24,18 @@ for _ in range(n):
     mana_points = int(input_line[2])
     heroes_dict[name] = {'hp': hit_points, 'mp': mana_points}
 
-print(heroes_dict)
+while True:
+    command, *params = input().split(" - ")
+    if command == "End":
+        break
+
+    elif command == "CastSpell":
+        hero_name = params[0]
+        mp_needed = int(params[1])
+        spell_name = params[2]
+        heroes_dict = cast_spell(heroes_dict, hero_name, mp_needed, spell_name)
+
+    elif command == "TakeDamage":
+        hero_name = params[0]
+        damage_points = int(params[1])
+        attacker_name = params[2]
