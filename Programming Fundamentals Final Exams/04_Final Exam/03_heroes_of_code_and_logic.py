3a43b8c231a1780307dabbb7ed47e3fe45adb443
Mihail Zhelyazkov <135510186+mihail859@users.noreply.github.com>
2023-11-29 19:10:58 +0200

    Update 03_heroes_of_code_and_logic.py

--- Programming Fundamentals Final Exams/04_Final Exam/03_heroes_of_code_and_logic.py
+++ Programming Fundamentals Final Exams/04_Final Exam/03_heroes_of_code_and_logic.py
@@ -4,8 +4,8 @@ heroes_dict = dict()
 for _ in range(n):
     input_line = input().split()
     name = input_line[0]
-    hp = int(input_line[1])
-    mp = int(input_line[2])
-    heroes_dict[name] = {'hp': hp, 'mp': mp}
+    hit_points = int(input_line[1])
+    mana_points = int(input_line[2])
+    heroes_dict[name] = {'hp': hit_points, 'mp': mana_points}
 
 print(heroes_dict)
